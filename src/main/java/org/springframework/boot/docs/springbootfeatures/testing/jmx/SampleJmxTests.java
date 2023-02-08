0587362b0203a8c4cb52f8b7bbc7692213b94dc8
bluesky <luversof@hanmail.net>
2023-02-08 21:13:30 +0900

    version, groupId 제거

--- src/main/java/org/springframework/boot/docs/springbootfeatures/testing/jmx/SampleJmxTests.java
+++ src/main/java/org/springframework/boot/docs/springbootfeatures/testing/jmx/SampleJmxTests.java
@@ -16,20 +16,16 @@
 
 package org.springframework.boot.docs.springbootfeatures.testing.jmx;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import javax.management.MBeanServer;
 import javax.management.MalformedObjectNameException;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-@ExtendWith(SpringExtension.class)
 @SpringBootTest(properties = "spring.jmx.enabled=true")
 @DirtiesContext
 class SampleJmxTests {
